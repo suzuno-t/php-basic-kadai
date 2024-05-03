a948dbeedd234410a349bda5e1622a63a2bf5bbd
suzuno-t <155156196+suzuno-t@users.noreply.github.com>
2024-05-03 21:00:11 +0900

    kadai_005 first commit

---
+++ kadai_005/score.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+    // 変数に値を入れる
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+    // 配列の平均値を出力する
+    echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10)/10;
+    ?>
+  </P>
+  
+</body>
+</html>
