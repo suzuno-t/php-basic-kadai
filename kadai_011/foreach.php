97dfe432dbfa8449f1e75f4847f91e321f49a144
suzuno-t <155156196+suzuno-t@users.noreply.github.com>
2024-05-03 21:35:01 +0900

    kadai_011 first commit

---
+++ kadai_011/foreach.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+    // 配列に値を代入する
+    $vege_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+    // 連想配列のキーと値を出力する
+    foreach($vege_data as $key => $value){
+      echo "{$key}:{$value}<br>";
+    }
+    ?>
+  </P>
+</body>
+</html>
